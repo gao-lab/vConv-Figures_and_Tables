6957656f055dcf0ee9823ffd7da1abc3d6dc2299
sybwjdnr <lijy@mail.cbi.pku.edu.cn>
2021-04-29 14:58:30 +0800

    rename script

---
+++ vConvFigmain/code/generate_supplementary_figure_11.R
@@ -0,0 +1,18 @@
+library("ggpubr")
+library("data.table")
+library("magrittr")
+library("foreach")
+
+
+dir.create("./vConvFigmain/result/Supplementary.Fig.11/", recursive=TRUE)
+
+auroc.dt <- foreach(temp.dataset=c("2", "4", "6", "8", "TwoDif1", "TwoDif2", "TwoDif3")) %do% {
+    data.table(
+        dataset=temp.dataset,
+        vConv.AUROC=paste(sep="", "./vConvFigmain/files.SF11/JasperMotif/SHLcompare/", temp.dataset, "_vCNN_auc.txt") %>% {scan(file=., what=double())},
+        vConvNoSHL.AUROC=paste(sep="", "./vConvFigmain/files.SF11/JasperMotif/SHLcompare/", temp.dataset, "_vCNNNSHL_auc.txt") %>% {scan(file=., what=double())}
+    ) %>% {.[, AUROC.increment:=vConv.AUROC-vConvNoSHL.AUROC]}} %>%
+    rbindlist %>%
+    {.[, dataset.to.plot:=c("2"="2 motifs", "4"="4 motifs", "6"="6 motifs", "8"="8 motifs", "TwoDif1"="TwoDiffMotif1", "TwoDif2"="TwoDiffMotif2", "TwoDif3"="TwoDiffMotif3")[dataset]]}
+
+temp.ggplot <- ggbarplot(data=auroc.dt, x="dataset.to.plot", y="AUROC.increment", fill="#53C1A5", palette="npg", add="mean_se") + labs(x="", y="Improvement in AUROC\nby enabling Shannon loss\nfor vConv-based network") + theme(axis.text.x=element_text(angle=45, hjust=1)); temp.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.11/Supplementary.Fig.11.png", plot=., device="png", width=14, height=12, units="cm")}
--- vConvFigmain/code/generate_supplementary_figure_5_new.R
+++ vConvFigmain/code/generate_supplementary_figure_5_new.R
@@ -4,13 +4,13 @@ library("magrittr")
 library("foreach")
 
 
-dir.create("./vConvFigmain/result/Supplementary.Fig.5/", recursive=TRUE)
+dir.create("./vConvFigmain/result/Supplementary.Fig.6/", recursive=TRUE)
 
 auroc.dt <- foreach(temp.dataset=c("2", "4", "6", "8", "TwoDif1", "TwoDif2", "TwoDif3")) %do% {
     data.table(
         dataset=temp.dataset,
-        CNN.AUROC=paste(sep="", "./vConvFigmain/files.SF5/JasperMotif/", temp.dataset, "_CNN_auc.txt") %>% {scan(file=., what=double())},
-        vConv.AUROC=paste(sep="", "./vConvFigmain/files.SF5/JasperMotif/", temp.dataset, "_vCNN_auc.txt") %>% {scan(file=., what=double())}
+        CNN.AUROC=paste(sep="", "./vConvFigmain/files.SF6/JasperMotif/", temp.dataset, "_CNN_auc.txt") %>% {scan(file=., what=double())},
+        vConv.AUROC=paste(sep="", "./vConvFigmain/files.SF6/JasperMotif/", temp.dataset, "_vCNN_auc.txt") %>% {scan(file=., what=double())}
     ) %>% {.[, index:=.I]} %>% {.[, pct.of.vConv.is.better.to.plot:=(sum(vConv.AUROC > CNN.AUROC)/.N) %>% round(4) %>% {.*100}]}} %>%
     rbindlist %>%
     {.[, dataset.to.plot:=c("2"="2 motifs", "4"="4 motifs", "6"="6 motifs", "8"="8 motifs", "TwoDif1"="TwoDiffMotif1", "TwoDif2"="TwoDiffMotif2", "TwoDif3"="TwoDiffMotif3")[dataset]]} %>%
@@ -35,22 +35,22 @@ temp.ggplot <- ggscatter(data=auroc.dt, x="vConv.AUROC", y="CNN.AUROC", size=1, 
     geom_abline(slope=1, intercept=c(0, 0)) +
     geom_hline(aes(yintercept=CNN.AUROC.lower.outlier.threshold), color="#4DBBD5", linetype="dashed") + 
     geom_vline(aes(xintercept=vConv.AUROC.lower.outlier.threshold), color="#E64B35", linetype="dashed") + 
-    facet_wrap(~title, ncol=2) + lims(x=c(0.0, 1), y=c(0.0, 1)); temp.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.new.png", plot=., device="png", width=12, height=23.5, units="cm")}
+    facet_wrap(~title, ncol=2) + lims(x=c(0.0, 1), y=c(0.0, 1)); temp.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.6/Supplementary.Fig.6.new.png", plot=., device="png", width=12, height=23.5, units="cm")}
 
 
 temp.no.dashed.lines.ggplot <- ggscatter(data=auroc.dt, x="vConv.AUROC", y="CNN.AUROC", size=1, alpha=0.2, color="#53C1A5", palette="npg") +
     labs(x="vConv-based network's AUROC", y="convolution-based\nnetwork's AUROC") +
     geom_abline(slope=1, intercept=c(0, 0)) +
-    facet_wrap(~title, ncol=2) + lims(x=c(0.0, 1), y=c(0.0, 1)); temp.no.dashed.lines.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.new.no.dashed.lines.png", plot=., device="png", width=12, height=23.5, units="cm")}
+    facet_wrap(~title, ncol=2) + lims(x=c(0.0, 1), y=c(0.0, 1)); temp.no.dashed.lines.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.6/Supplementary.Fig.6.new.no.dashed.lines.png", plot=., device="png", width=12, height=23.5, units="cm")}
 
 temp.no.dashed.lines.0.4.to.1.0.ggplot <- ggscatter(data=auroc.dt, x="vConv.AUROC", y="CNN.AUROC", size=1, alpha=0.2, color="#53C1A5", palette="npg") +
     labs(x="vConv-based network's AUROC", y="convolution-based\nnetwork's AUROC") +
     geom_abline(slope=1, intercept=c(0, 0)) +
-    facet_wrap(~title, ncol=2) + lims(x=c(0.4, 1), y=c(0.4, 1)); temp.no.dashed.lines.0.4.to.1.0.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.new.no.dashed.lines.0.4.to.1.0.png", plot=., device="png", width=12, height=23.5, units="cm")}
+    facet_wrap(~title, ncol=2) + lims(x=c(0.4, 1), y=c(0.4, 1)); temp.no.dashed.lines.0.4.to.1.0.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.6/Supplementary.Fig.6.new.no.dashed.lines.0.4.to.1.0.png", plot=., device="png", width=12, height=23.5, units="cm")}
 
 
 temp.no.dashed.lines.0.4.to.1.0.3.col.ggplot <- ggscatter(data=auroc.dt, x="vConv.AUROC", y="CNN.AUROC", size=1, alpha=0.2, color="#53C1A5", palette="npg") +
     labs(x="vConv-based network's AUROC", y="convolution-based\nnetwork's AUROC") +
     geom_abline(slope=1, intercept=c(0, 0)) +
-    facet_wrap(~title, ncol=3) + lims(x=c(0.4, 1), y=c(0.4, 1)); temp.no.dashed.lines.0.4.to.1.0.3.col.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.new.no.dashed.lines.0.4.to.1.0.3.col.png", plot=., device="png", width=18, height=18.5, units="cm")}
+    facet_wrap(~title, ncol=3) + lims(x=c(0.4, 1), y=c(0.4, 1)); temp.no.dashed.lines.0.4.to.1.0.3.col.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.6.new.no.dashed.lines.0.4.to.1.0.3.col.png", plot=., device="png", width=18, height=18.5, units="cm")}
 
