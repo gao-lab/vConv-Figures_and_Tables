09a616615895fe2fcf67be6702e8a09c2f591e6d
Yang Ding <dingyang_dingyang@163.com>
2021-04-30 10:38:21 +0800

    clean SF5 script

--- vConvFigmain/code/generate_supplementary_figure_5.R
+++ vConvFigmain/code/generate_supplementary_figure_5.R
@@ -1,34 +1,25 @@
 library("ggpubr")
 library("data.table")
 library("magrittr")
-library("readxl")
 library("foreach")
-library("ggseqlogo")
+
 
 dir.create("./vConvFigmain/result/Supplementary.Fig.5/", recursive=TRUE)
 
-pwm.list.collection <- read.csv("./vConvFigmain/files.SF5/tables.csv", na="NA") %>%
-    data.table %>%
-    {melt(data=., id.vars="motif", measure.vars=c("cnn.kernel", "ground.truth", "vconv.kernel"), variable.name="type", value.name="filename")} %>%
-    {
-        all.types <- .[, unique(type)];
-        foreach(temp.type=all.types, .final=function(temp.x){setNames(temp.x, all.types)}) %do% {
-            all.motifs <- .[type==temp.type][, unique(motif)];
-            foreach(temp.motif=all.motifs, .inorder=TRUE, .final=function(temp.x){setNames(temp.x, all.motifs)}) %do% {
-                if (is.na(.[type==temp.type & motif == temp.motif][1, filename]) == TRUE){
-                    matrix(0.25, nrow=4, ncol=14) %>% {rownames(.) <- c("A", "C", "G", "T"); .} %>% {apply(.+1e-10, MARGIN=2, FUN=function(temp.col){(log2(4) + sum(temp.col * log2(temp.col))) * temp.col})}
-                } else {
-                    .[type==temp.type & motif == temp.motif][1, filename] %>% read.table %>% as.matrix %>% {colnames(.) <- c("A", "C", "G", "T"); .} %>% t %>% {apply(.+1e-10, MARGIN=2, FUN=function(temp.col){(log2(4) + sum(temp.col * log2(temp.col))) * temp.col})}
-                }
-            }
-        }
-    }
+auroc.dt <- foreach(temp.dataset=c("2", "4", "6", "8", "TwoDif1", "TwoDif2", "TwoDif3")) %do% {
+    data.table(
+        dataset=temp.dataset,
+        vConv.AUROC=paste(sep="", "./vConvFigmain/files.F2/JasperMotif/", temp.dataset, "_vCNNbestRandom_auc.txt") %>% {scan(file=., what=double())},
+        CNN.AUROC=paste(sep="", "./vConvFigmain/files.F2/JasperMotif/", temp.dataset, "_CNNbestRandom_auc.txt") %>% {scan(file=., what=double())}
+    ) %>% {.[, AUROC.increment:=vConv.AUROC-CNN.AUROC]} %>% {.[, index:=.I]} } %>%
+    rbindlist %>%
+    {.[, dataset.to.plot:=c("2"="2 motifs", "4"="4 motifs", "6"="6 motifs", "8"="8 motifs", "TwoDif1"="TwoDiffMotif1", "TwoDif2"="TwoDiffMotif2", "TwoDif3"="TwoDiffMotif3")[dataset]]}
 
-pwm.list.collection %>%
-    {list(
-         ggplot() + geom_logo(.[["ground.truth"]], method="custom", seq_type="dna") + theme_logo() + facet_grid(seq_group~"Inserted motif", switch="y") + theme(strip.placement="outside", axis.ticks=element_line(), axis.line=element_line()) + lims(y=c(0, 2)),
-         ggplot() + geom_logo(.[["vconv.kernel"]], method="custom", seq_type="dna") + theme_logo() + facet_grid(seq_group~"vConv-based", switch="y") + theme(strip.text.y=element_blank(), axis.ticks=element_line(), axis.line=element_line()) + lims(y=c(0, 2)),
-         ggplot() + geom_logo(.[["cnn.kernel"]], method="custom", seq_type="dna") + theme_logo() + facet_grid(seq_group~"Canonical convolution-based", switch="y") + theme(strip.text.y=element_blank(), axis.ticks=element_line(), axis.line=element_line()) + geom_text(data=data.table(seq_group="MA0234.1", x=8, y=1, label="Not found"), mapping=aes(x=x, y=y, label=label), size=10) + lims(y=c(0, 2))
-     )} %>%
-    {ggarrange(plotlist=., ncol=3)} %>%
-    {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.png", plot=., device="png", width=24, height=16, units="cm")}
+auroc.melt.dt <- melt(data=auroc.dt, id.vars=c("dataset", "dataset.to.plot", "index"), measure.vars=c("vConv.AUROC", "CNN.AUROC"), variable.name="model", value.name="AUROC")[, model.to.plot := c("vConv.AUROC"="vConv-based", "CNN.AUROC"="convolution-based")[model]]
+
+SF5.ggplot <- ggboxplot(data=auroc.melt.dt, x="model.to.plot", y="AUROC", fill="model.to.plot", palette="npg") + labs(x="", y="\n\nAUROC", fill="") + facet_grid(~dataset.to.plot) + theme(axis.text.x=element_blank())
+
+## F2B.ggplot <- ggbarplot(data=auroc.dt, x="dataset.to.plot", y="AUROC.increment", fill="#53C1A5", palette="npg", add="mean_se") + labs(x="", y="Improvement in AUROC by\nvConv-based network") + theme(axis.text.x=element_text(angle=45, hjust=1))
+
+SF5.ggplot %>% {ggsave(filename="./vConvFigmain/result/Supplementary.Fig.5/Supplementary.Fig.5.png", plot=., device="png", width=20, height=18, units="cm")}
+
